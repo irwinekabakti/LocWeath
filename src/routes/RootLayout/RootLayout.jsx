264a591e4eb9bc269f30a9bdca91ff52355f4555
irwinekabakti <irwinekabakti@gmail.com>
2023-05-27 13:56:32 +0700

    feat:(navbar)

--- src/routes/RootLayout/RootLayout.jsx
+++ src/routes/RootLayout/RootLayout.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../../theme/theme";
+import { DashboardContainer, MiddleContainer } from "../../theme/Styled";
 import TopNav from "../../components/TopNav";
 import Footer from "../../components/Footer";
 import LeftNav from "../../components/LeftNav";
-import Searchbar from "../../components/Searchbar";
 
 const RootLayout = () => {
   return (
     <>
-      <TopNav />
-      <LeftNav />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <TopNav />
+        <DashboardContainer>
+          <LeftNav />
+        </DashboardContainer>
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 };
